d1b06680386621323e74229bcb317096ca6dc752
Daraganov Git-71 <denis.daraganov2013@yandex.ru>
2023-08-08 17:51:38 +0300

    add TestNg

---
+++ src/test/java/ru/netology/service/CashBackHackServiceTest.java
@@ -0,0 +1,30 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashBackHackServiceTest {
+
+    CashBackHackService service = new CashBackHackService();
+
+    @Test
+    public void ifAmount900() {
+        assertEquals(service.remain(999), 1);
+    }
+
+    @Test
+    public void ifAmount0() {
+        assertEquals(service.remain(1), 999);
+    }
+
+    @Test
+    public void ifAmount4500() {
+        assertEquals(service.remain(4500), 500);
+    }
+
+    @Test
+    public void ifAmount1000() { //при покупке на 1000, сервис должен советовать добавить 0р. бат вос 1000
+        assertEquals(service.remain(1000), 0);
+    }
+}
